fe487fcfe8afa19ac1e7f8102bf9657b1278064e
ad <andre.dossinger@sap.com>
2023-10-06 07:59:22 +0200

    adding configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -98,8 +98,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    curl
+    jq
     neovim
     alacritty
     git
