890c37be6979fdd9df974d7a4dfc9184b89c9917
ad <andre.dossinger@sap.com>
2023-11-07 09:02:27 +0100

    fixing dhcp issue

--- configuration.nix
+++ configuration.nix
@@ -22,7 +22,12 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager = {
+      enable = true;
+      dhcp = "dhcpcd";
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
