25a7387ffa2228cb5de2c68d7e597feb7dfaa94c
ad <mail@andre-dossinger.com>
2025-11-17 21:49:33 +0100

    strudel

--- .config/nvim/lua/plugins/strudel.lua
+++ .config/nvim/lua/plugins/strudel.lua
@@ -4,4 +4,10 @@ return {
   config = function()
     require("strudel").setup()
   end,
+  keys = {
+    { "<leader>tl", "<cmd>StrudelLaunch<cr>", desc = "Launch Strudel.cc" },
+    { "<leader>tq", "<cmd>StrudelQuit<cr>", desc = "Quit Strudel.cc" },
+    { "<leader>tu", "<cmd>StrudelUpdate<cr>", desc = "Triggers code evaluation" },
+    { "<leader>tt", "<cmd>StrudelToggle<cr>", desc = "Start/Stop code evaluation" },
+  },
 }
